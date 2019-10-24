403b6a61c2e09b9ea7b8bce6cffb11bfdc7ea495
Emery Tate <tateejt@gmail.com>
2019-10-24 05:21:49 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,13 @@
 class Backer
   
   attr_accessor :name, :backed_projects
+  
   def initialize(name)
     @name = name
     @backed_projects = []
   end
   
-  def backed_projects(project)
+  def back_project(project)
     @backed_projects << project
     project.add_backer(self) unless project.backers.include?(self)
   end
